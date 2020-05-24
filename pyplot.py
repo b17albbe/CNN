b35892fcca92dbb297f88d57065438d4cf26abe5
b17albbe <38030926+b17albbe@users.noreply.github.com>
2020-05-24 18:14:01 +0200

    pics

--- pyplot.py
+++ pyplot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-plt.plot([0, 1, 2, 3, 4, 5], [0, 23, 33, 37, 41, 46], marker='o')
+plt.plot([0, 1, 2, 3, 4, 5, 6], [0, 23, 33, 37, 41, 46], marker='o')
 plt.plot([0, 1, 2, 3, 4, 5], [0, 17, 27, 34, 39, 42], marker='o')
 plt.axis([0, 5, 0, 100])
 plt.ylabel('Accuracy %')
